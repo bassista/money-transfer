d31d087dd5964f9c54b35c5fec239e98fcd0d033
Marco Sabatini <marco.sabatini@external-rumbo.com>
2018-02-22 17:00:31 +0100

    some test refactoring.

--- src/test/java/com/github/msabatini/api/AcceptanceTest.java
+++ src/test/java/com/github/msabatini/api/AcceptanceTest.java
@@ -19,6 +19,13 @@ import static org.junit.Assert.assertThat;
 public class AcceptanceTest
 {
   private static final String TRANSFER_PATH = "v1/customers/{customerId}/transfers";
+  private static final String CUSTOMER_ID_PARAMETER = "customerId";
+  private static final String CUSTOMER_ID_VALUE = "customer1";
+  private static final String EUR = "EUR";
+  private static final String GBP = "GBP";
+  private static final String NOT_EXISTENT_CURRENCY = "notExistent";
+  private static final String INVALID_AMOUNT = "-1";
+  private static final String HUGE_AMOUNT = "100";
 
   private HttpServer server;
   private WebTarget target;
@@ -39,14 +46,13 @@ public class AcceptanceTest
   @Test
   public void transferAccepted()
   {
-    Response response =
-      target.path(TRANSFER_PATH)
-            .resolveTemplate("customerId", "customer1")
-            .request()
-            .post(json(aTransferRequest()
-                         .withAccountFrom("EUR")
-                         .withAccountTo("GBP")
-                         .build()));
+    Response response = target.path(TRANSFER_PATH)
+                              .resolveTemplate(CUSTOMER_ID_PARAMETER, CUSTOMER_ID_VALUE)
+                              .request()
+                              .post(json(aTransferRequest()
+                                             .withAccountFrom(EUR)
+                                             .withAccountTo(GBP)
+                                             .build()));
 
     assertThat(response.getStatus(), is(202));
     assertThat(response.readEntity(String.class), is("Transfer is accepted"));
@@ -55,50 +61,45 @@ public class AcceptanceTest
   @Test
   public void accountNotFound()
   {
-    Response response =
-      target.path(TRANSFER_PATH)
-            .resolveTemplate("customerId", "customer1")
-            .request()
-            .post(json(aTransferRequest()
-                         .withAccountFrom("notExistent")
-                         .build()));
+    Response response = target.path(TRANSFER_PATH)
+                              .resolveTemplate(CUSTOMER_ID_PARAMETER, CUSTOMER_ID_VALUE)
+                              .request()
+                              .post(json(aTransferRequest()
+                                             .withAccountFrom(NOT_EXISTENT_CURRENCY)
+                                             .build()));
 
-    assertThat(response.readEntity(String.class), is("Account with id notExistent not found"));
     assertThat(response.getStatus(), is(404));
+    assertThat(response.readEntity(String.class), is("Account with id notExistent not found"));
   }
 
   @Test
   public void insufficientBalance()
   {
-    Response response =
-      target.path(TRANSFER_PATH)
-            .resolveTemplate("customerId", "customer1")
-            .request()
-            .post(json(aTransferRequest()
-                         .withAccountFrom("EUR")
-                         .withAccountTo("GBP")
-                         .withAmount("100")
-                         .build()));
+    Response response = target.path(TRANSFER_PATH)
+                              .resolveTemplate(CUSTOMER_ID_PARAMETER, CUSTOMER_ID_VALUE)
+                              .request()
+                              .post(json(aTransferRequest().withAccountFrom(EUR)
+                                                           .withAccountTo(GBP)
+                                                           .withAmount(HUGE_AMOUNT)
+                                                           .build()));
 
-    assertThat(response.readEntity(String.class), containsString("Balance insufficient"));
     assertThat(response.getStatus(), is(400));
+    assertThat(response.readEntity(String.class), containsString("Balance insufficient"));
   }
 
   @Test
   public void aWrongRequest()
   {
-    Response response =
-      target.path(TRANSFER_PATH)
-            .resolveTemplate("customerId", "customer1")
-            .request()
-            .post(json(aTransferRequest()
-                         .withAccountFrom("EUR")
-                         .withAccountTo("GBP")
-                         .withAmount("-1")
-                         .build()));
+    Response response = target.path(TRANSFER_PATH)
+                              .resolveTemplate(CUSTOMER_ID_PARAMETER, CUSTOMER_ID_VALUE)
+                              .request()
+                              .post(json(aTransferRequest().withAccountFrom(EUR)
+                                                           .withAccountTo(GBP)
+                                                           .withAmount(INVALID_AMOUNT)
+                                                           .build()));
 
-    assertThat(response.readEntity(String.class), is("Invalid value EUR -1. It has to be positive"));
     assertThat(response.getStatus(), is(500));
+    assertThat(response.readEntity(String.class), is("Invalid value EUR -1. It has to be positive"));
   }
 
 }
